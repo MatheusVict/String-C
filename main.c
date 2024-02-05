b1ee3325a5049886f5f1b739e04b9964c5fe7cc7
MatheusVict <matheusvictorhenrique@gmail.com>
2024-02-05 17:42:52 -0300

    feat: new scanner
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // Strings are a char vector
+    // char == 8 bits
+
+    char s[10];
+
+    printf("Say something (conventional scanner)\n");
+    scanf("%s", s);
+    fflush(stdin);// clean keyboard
+
+    printf("Result %s\n\n", s);
+
+
+    printf("Say something else (appreciatory scanner)\n");
+    scanf("%9[^\n]s", s);
+    fflush(stdin);
+
+    printf("Result %s\n\n", s);
     return 0;
 }
